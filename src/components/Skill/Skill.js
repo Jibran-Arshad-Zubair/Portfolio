464037d562d7be5b5da0c1660fcc4c2587a1b952
Jibran Arshad <ranajibranarshad@gmail.com>
2025-09-13 19:36:12 +0500

    Update Skills Page

--- src/components/Skill/Skill.js
+++ src/components/Skill/Skill.js
@@ -1,37 +1,38 @@
 import React, { useEffect } from 'react';
-
-// Importing TagCloud for  3D Rotating Text Sphere
 import TagCloud from 'TagCloud';
 
 import '../../styles/Skill/Skill.scss';
 
 const Skill = () => {
-  // Animation settings for Text Cloud
+
   useEffect(() => {
     const container = '.tagcloud';
     let radii;
 
-    // If 3D Text Sphere is not showing up after deployment remove the return (i.e, return() =>{}) function from below code
     return () => {
       const texts = [
-        'HTML',
-        'CSS',
-        'Bootsrap',
         'JavaScript',
-        'ReactJs',
-        'NextJs',
-        'NodeJS',
-        'Jquery',
-        'ES6',
-        'GIT',
-        'GITHUB',
+        'TypeScript',
+        'React.js',
+        'Next.js',
+        'Node.js',
+        'Express.js',
         'MongoDB',
-        'ExpressJs',
-        
+        'Tailwind CSS',
+        'Bootstrap 5',
+        'RTK',
+        'Zustand',
+        'TanStack',
+        'Socket.io',
+        'REST API',
+        'Git',
+        'GitHub',
+        'GenAI',
+        'OpenAI Integration',
+        'Google Cloud',
+        'AWS',
       ];
 
-
-      // Decrasing 'radius' value for small screen devices  
       function radiusValue() {
         if (window.screen.width <= 778) {
           radii = 150;
@@ -49,118 +50,67 @@ const Skill = () => {
       };
 
       TagCloud(container, texts, options);
-    }
+    };
   }, []);
+
   return (
     <>
       <div className="skill">
         <div className="skill__main-container">
           <div className="skill__header" id="skill__heading">
             <h1>
-              <span
-                data-aos="fade-down"
-                data-aos-delay="0"
-                data-aos-offset="200"
-              >
-                S
-              </span>
-              <span
-                data-aos="fade-down"
-                data-aos-delay="200"
-                data-aos-offset="200"
-              >
-                K
-              </span>
-              <span
-                data-aos="fade-down"
-                data-aos-delay="400"
-                data-aos-offset="200"
-              >
-                I
-              </span>
-              <span
-                data-aos="fade-down"
-                data-aos-delay="600"
-                data-aos-offset="200"
-              >
-                L
-              </span>
-              <span
-                data-aos="fade-down"
-                data-aos-delay="800"
-                data-aos-offset="200"
-              >
-                L
-              </span>
-              <span
-                data-aos="fade-down"
-                data-aos-delay="1000"
-                data-aos-offset="200"
-              >
-                S
-              </span>
+              <span data-aos="fade-down" data-aos-delay="0" data-aos-offset="200">S</span>
+              <span data-aos="fade-down" data-aos-delay="200" data-aos-offset="200">K</span>
+              <span data-aos="fade-down" data-aos-delay="400" data-aos-offset="200">I</span>
+              <span data-aos="fade-down" data-aos-delay="600" data-aos-offset="200">L</span>
+              <span data-aos="fade-down" data-aos-delay="800" data-aos-offset="200">L</span>
+              <span data-aos="fade-down" data-aos-delay="1000" data-aos-offset="200">S</span>
             </h1>
           </div>
+
           <div className="skill__inner-container">
             <div className="skill__info">
               <div className="skill__details-container">
+                
+               
                 <div className="skill__details">
-                  <h4>HTML / CSS</h4>
+                  <h4>Frontend</h4>
                   <ul>
-                    <li>
-                      <span>HTML5, CSS3 ,Bootstrap ,Tailwind CSS</span>
-                    </li>
+                    <li><span>HTML5, CSS3</span></li>
+                    <li><span>Bootstrap 5, Tailwind CSS</span></li>
+                    <li><span>JavaScript (ES6+), TypeScript</span></li>
+                    <li><span>React.js, Next.js</span></li>
+                    <li><span>RTK, Zustand, TanStack</span></li>
                   </ul>
                 </div>
+
                 <div className="skill__details">
-                  <h4>JavaScript</h4>
+                  <h4>Backend</h4>
                   <ul>
-                    <li>
-                      <span>
-                        <b>Frameworks & Libraries:</b> &nbsp; React JS, Next JS
-                      </span>
-                    </li>
-                    <li>
-                      <span>ES5/ES6</span>
-                    </li>
-                    <li>
-                      <span>Node.js</span>
-                    </li>
-                    <li>
-                      <span>jQuery</span>
-                    </li>
-                    <li>
-                      <span>Typescript</span>
-                    </li>
+                    <li><span>Node.js</span></li>
+                    <li><span>Express.js</span></li>
+                    <li><span>MongoDB</span></li>
+                    <li><span>Socket.io</span></li>
+                    <li><span>REST API Development</span></li>
                   </ul>
                 </div>
+
+               
                 <div className="skill__details">
-                  <h4>Version Control Systems</h4>
+                  <h4>Tools & Platforms</h4>
                   <ul>
-                    <li>
-                      <span>GIT</span> <br></br>
-                      <li>
-                      <span>GITHUB</span>
-                      </li>
-                      
-                    </li>
+                    <li><span>Git & GitHub</span></li>
+                    <li><span>Google Cloud</span></li>
+                    <li><span>AWS</span></li>
                   </ul>
                 </div>
+
+               
                 <div className="skill__details">
-                 
-                </div>
-                <div className="skill__details">
-                  <h4>Server Side</h4>
+                  <h4>AI & Integrations</h4>
                   <ul>
-                    <li>
-                      <span>Node.js</span>
-                    </li>
-                    <li>
-                      <span>Express.js</span>
-                    </li>
-                    <li>
-                      <span>MongoDB</span>
-                    </li>
+                    <li><span>GenAI</span></li>
+                    <li><span>OpenAI Integration</span></li>
                   </ul>
                 </div>
               </div>
